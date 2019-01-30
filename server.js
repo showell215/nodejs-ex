214ad0817d1406039557628cfff56788e8e86139
Steven Howell <showell@us.ibm.com>
2019-01-30 13:25:10 -0500

    remove old port env var

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || 8080,
+    ip   = process.env.IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
